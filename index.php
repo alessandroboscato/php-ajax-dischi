6dad0193dfbe252eb4054b0b14d916426cd26cea
Alessandro <alessandro.boscato@gmail.com>
2020-10-07 14:59:10 +0200

    laravel mix

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Php-ajax-dischi</title>
+  <link rel="stylesheet" href="dist/app.css">
+</head>
+<body>
+
+</body>
+</html>
